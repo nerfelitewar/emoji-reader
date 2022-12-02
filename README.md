1bf3d08434ed77ea1580247be1ed52ae500e4b21
nerfelitewar <md.arshadrazanoori23@gmail.com>
2022-12-03 01:47:00 +0530

    commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# emoji-reader
+# emoji-reader or EMOTION READER
 This will read emoji and feelings of the persons using AI 
+
+> READ A FILE OR IMAGE (OCR)
+
+> SEE FOR ALL EMOJIES USED 
+
+> USE COMBINATION AND LOGIC TO HOW THAT PERSON IS FEELING 
+
+> READ OUT THE MESSAGE IF "YES" BY USER
+
+> SHOW HOW THAT PERSON IS FEELING AND TONE LIKE- ... (not confident) etc. all emotions
+
+
--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 from itertools import permutations , combinations
 import pyautogui as pg
+import emoji as emj
 import sys 
-import os 
+import pyttsx3
+import random
+
+with open('text.txt','w',encoding="utf-8") as f:
+    w=input('Say ur msg- ')
+    sys.stdout.reconfigure(encoding='utf-8')
+    #w=pg.prompt("Say your msg",title="MSG")
+
+    w=emj.demojize(w)
+
+
+    f.writelines(w)
+    f.close()
+
+
+with open('text.txt') as r:
+    data=r.read()
+    data_reading=emj.emojize(data)
+    Engine=pyttsx3.init()
+    print(data_reading)
+
+    data
+
+    Engine.say(data)
+
+    Engine.runAndWait()
+
+    
+    
+
+
+
 
 
 
