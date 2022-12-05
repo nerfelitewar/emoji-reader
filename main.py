7d69cf265071378c5bf091b966ffce5488ab9b42
nerfelitewar <md.arshadrazanoori23@gmail.com>
2022-12-06 04:42:38 +0530

    done

--- main.py
+++ main.py
@@ -32,10 +32,10 @@ with open('text.txt') as r:
     print(list_emotion) #all emoji list in a txt file 
     #NOTE- Maybe i should use {} sets so that no repetition ^^^
     
-    str_emotion=[]
-    for j in list_emotion:
+    str_emotion=["The person is feeling "]
+    for j in set(list_emotion):
         emo_val=random.choice(obj.get(j)) #emoji feelings from the list
-        str_emotion.append(emo_val)
+        str_emotion.append(emo_val+" ")
     
 
 
@@ -43,12 +43,13 @@ with open('text.txt') as r:
     Engine=pyttsx3.init()
     print(data_reading)
     read=data.replace("_",' ')
-    Engine.say(read) #should not say underscore bs WORKS :D 
+    Engine.say(read) #avoid saying underscore. NOTE- If user meant to put underscore? email_id: etc.
 
+    ###str_emotion=say_emotion
+    #say_emotion=("Person is feeling "+str(str_emotion))
 
-    say_emotion=("Person is feeling "+str(str_emotion))
-    print(say_emotion)
-    Engine.say(say_emotion)
+    print(str_emotion)
+    Engine.say(str_emotion)
     Engine.runAndWait()
 
 
