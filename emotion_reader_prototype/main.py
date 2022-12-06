86143aeb8c3662998c10f897b9f4b0152792d12d
nerfelitewar <md.arshadrazanoori23@gmail.com>
2022-12-06 05:37:00 +0530

    prototype done

--- emotion_reader_prototype/main.py
+++ emotion_reader_prototype/main.py
@@ -1,14 +1,14 @@
-from itertools import permutations , combinations
 import pyautogui as pg
 import emoji as emj
 import sys 
+from colorama import Fore 
 import pyttsx3
 from emotion import emoji_emotion
 import random
 from ordered_set import OrderedSet
 
 with open('text.txt','w',encoding="utf-8") as f:
-    w=input('Say ur msg- ')
+    w=input('Enter your message-')
 
     w=emj.demojize(w)
 
@@ -28,23 +28,26 @@ with open('text.txt') as r:
         if emj.is_emoji(i)==True:
             list_emotion.append(i)
 
-    print(list_emotion) #all emoji list in a txt file 
+    ###print(list_emotion) #all emoji list in a txt file 
     #NOTE- Maybe i should use {} sets so that no repetition ^^^
     
-    str_emotion=["The person is feeling "]
+    str_emotion=["The person is feeling-"]
     for j in OrderedSet(list_emotion): #unique orderset of emojies from list 
         emo_val=random.choice(obj.get(j)) #emoji feelings from the list
-        str_emotion.append(emo_val+" ")
+        str_emotion.append(emo_val+",")
+
+
+    print(Fore.LIGHTRED_EX+data_reading+Fore.RESET)
     
+    for emo in str_emotion:
+        print(Fore.LIGHTBLACK_EX+emo,end=" "+Fore.RESET)
 
 
-
-    Engine=pyttsx3.init()
-    print(data_reading)
     read=data.replace("_",' ')
-    Engine.say(read) #avoid saying underscore. NOTE- If user meant to put underscore? email_id: etc.
 
-    print(str_emotion)
+    
+    Engine=pyttsx3.init() #init for module pyttsx3
+    Engine.say(read) #avoid saying underscore. NOTE- If user meant to put underscore? email_id: etc.
     Engine.say(str_emotion)
     Engine.runAndWait()
 
