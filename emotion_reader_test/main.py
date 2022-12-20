b5b26e624eba29628e26ef210aa5fc0a9f74cbd5
nerfelitewar <md.arshadrazanoori23@gmail.com>
2022-12-20 14:46:04 +0530

    done changes

--- emotion_reader_test/main.py
+++ emotion_reader_test/main.py
@@ -1,15 +1,15 @@
-import pyautogui as pg
+#import pyautogui as pg
 import emoji as emj
-import sys 
+#import sys 
 from colorama import Fore 
 import pyttsx3
 from emotion import emoji_emotion
 import random
 from ordered_set import OrderedSet
 
-with open('text.txt','w',encoding="utf-8") as f:
+with open('emotion_reader_test/text.txt','w',encoding="utf-8") as f:
     w=input('Enter your message-')
-
+    emj_cnt=emj.emoji_count(w)
     w=emj.demojize(w)
 
 
@@ -17,21 +17,21 @@ with open('text.txt','w',encoding="utf-8") as f:
     f.close()
 
 
-with open('text.txt') as r:
+with open('emotion_reader_test/text.txt') as r:
     data=r.read()
     data_reading=emj.emojize(data)
 
 
     obj=emoji_emotion.emj_emo #object for all emoji hash mapped
     list_emotion=[]
-    for i in data_reading:
+    for i in data_reading: 
         if emj.is_emoji(i)==True:
             list_emotion.append(i)
 
-    ###print(list_emotion) #all emoji list in a txt file 
-    #NOTE- Maybe i should use {} sets so that no repetition ^^^
+
+
     
-    str_emotion=["The person is feeling-"]
+    str_emotion=["The person is feeling- "]
     for j in OrderedSet(list_emotion): #unique orderset of emojies from list 
         emo_val=random.choice(obj.get(j)) #emoji feelings from the list
         str_emotion.append(emo_val+",")
@@ -48,7 +48,10 @@ with open('text.txt') as r:
     
     Engine=pyttsx3.init() #init for module pyttsx3
     Engine.say(read) #avoid saying underscore. NOTE- If user meant to put underscore? email_id: etc.
-    Engine.say(str_emotion)
+    if emj_cnt!=0:
+        Engine.say(str_emotion)
+    else:
+        Engine.say("The person is feeling nothing, try reading text messages ")
     Engine.runAndWait()
 
 
